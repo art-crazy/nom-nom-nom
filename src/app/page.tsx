200b8ea5a894a8b4b56104c1c8ac4a81e2462472
arzhannikov <artcrazy.mail@gmail.com>
2025-06-02 01:20:51 +0300

    home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,8 @@
 import styles from './Home.module.scss';
-import { Breadcrumbs } from '@/components/Breadcrumbs/Breadcrumbs';
 
 export default function Home() {
   return (
     <div className={styles.home}>
-      <Breadcrumbs
-        title="Главная"
-        paths={[]}
-      />
     </div>
   );
 }
