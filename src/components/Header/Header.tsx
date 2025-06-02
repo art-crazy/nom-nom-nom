0c8b51b496c979fcfa10d10a425910b03b9bfec9
arzhannikov <artcrazy.mail@gmail.com>
2025-06-03 00:36:25 +0300

    add mobile nav

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,6 +4,7 @@ import { usePathname } from 'next/navigation';
 import Link from 'next/link';
 import Search from './Search/Search';
 import MobileNav from './MobileNav/MobileNav';
+import NavItem from '@/components/Navigation/NavItem';
 
 const Header = () => {
   const pathname = usePathname();
@@ -21,7 +22,13 @@ const Header = () => {
         <Link href="/" className={styles.logo} onClick={handleLogoClick} style={{ cursor: 'pointer' }}>
           🍴 QuickCook
         </Link>
-        <Search />
+        <div className={styles.headerRight}>
+          <Search />
+          <div className={styles.desktopNav}>
+            <NavItem type="recipes" className={styles.desktopNavItem} />
+            <NavItem type="collections" className={styles.desktopNavItem} />
+          </div>
+        </div>
       </header>
       <MobileNav />
     </>
--- src/components/Header/MobileNav/MobileNav.tsx
+++ src/components/Header/MobileNav/MobileNav.tsx
@@ -1,29 +1,12 @@
 import React from 'react';
-import Link from 'next/link';
-import { usePathname } from 'next/navigation';
 import styles from './MobileNav.module.scss';
-import RecipesIcon from '@/components/icons/RecipesIcon';
-import CollectionsIcon from '@/components/icons/CollectionsIcon';
+import NavItem from '@/components/Navigation/NavItem';
 
 const MobileNav: React.FC = () => {
-  const pathname = usePathname();
-
   return (
     <nav className={styles.mobileNav}>
-      <Link
-        href="/recepty"
-        className={`${styles.navItem} ${pathname === '/recepty' ? styles.active : ''}`}
-      >
-        <RecipesIcon className={styles.icon} />
-        <span>Все рецепты</span>
-      </Link>
-      <Link
-        href="/"
-        className={`${styles.navItem} ${pathname === '/' ? styles.active : ''}`}
-      >
-        <CollectionsIcon className={styles.icon} />
-        <span>Подборки</span>
-      </Link>
+      <NavItem type="recipes" className={styles.mobileNavItem} />
+      <NavItem type="collections" className={styles.mobileNavItem} />
     </nav>
   );
 };
