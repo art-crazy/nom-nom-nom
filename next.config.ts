243aa79c6b7a393467a7780edc7be708e72a6a63
arzhannikov <artcrazy.mail@gmail.com>
2025-06-18 00:59:54 +0300

    fix deploy yml

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'standalone',
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
+  },
+  poweredByHeader: false,
+  swcMinify: true
 };
 
 export default nextConfig;
