9e839f1fd48e8a3203caf7ee71137b24f96cb3f1
arzhannikov <artcrazy.mail@gmail.com>
2025-08-04 14:08:42 +0300

    ref

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -2,20 +2,35 @@ import React from 'react';
 import Link from 'next/link';
 import styles from './Footer.module.scss';
 
+const LINKS = [
+    {
+        text: 'Наша история',
+        url: "/istoriya",
+    },
+    {
+        text: 'Блог',
+        url: "/blog",
+    },
+    {
+        text: 'Вопросы и ответы',
+        url: "/faq",
+    },
+]
+
 export default function Footer() {
   return (
     <footer className={styles.footer}>
       <nav className={styles.section} aria-label="О нас">
         <b>О нас</b>
-        <Link href="/istoriya">Наша история</Link>
-        <Link href="/blog">Блог</Link>
-        <Link href="/faq">Вопросы и ответы</Link>
+          {LINKS.map((link, index) => (
+              <Link key={index} href={link.url}>{link.text}</Link>
+          ))}
       </nav>
-      <nav className={styles.section} aria-label="Помощь">
-        <b>Помощь</b>
-        <a href="#">Контакты</a>
-        <a href="#">Условия</a>
-      </nav>
+      {/*<nav className={styles.section} aria-label="Помощь">*/}
+      {/*  <b>Помощь</b>*/}
+      {/*  <a href="#">Контакты</a>*/}
+      {/*  <a href="#">Условия</a>*/}
+      {/*</nav>*/}
       <div className={styles.copyright}>
         © 2025 «Что приготовить». Все права защищены.
       </div>
